736c2a54dc9c1d4565d17e2d3fa4af09125a6cf4
Filip A. Zofakis <fil.zofakis@gmail.com>
2019-02-16 23:42:05 +0000

    Initializing quantstrat package variables

--- financial_trading_in_r.R
+++ financial_trading_in_r.R
@@ -1,5 +1,11 @@
 install.packages("remotes")
-remotes::install_ygithub("braverock/quantstrat")
+install.packages("devtools")
+require(devtools)
+install_github("braverock/blotter") # dependency
+remotes::install_github("braverock/quantstrat")
+
+# Load the quantmod package
+library(quantmod)
 
 # From quantmod library
 # Get SPY from yahoo
@@ -28,3 +34,37 @@ Sys.setenv(TZ = "UTC")
 
 # Set the currency to USD 
 currency("USD")
+
+# Retrieve SPY from yahoo
+getSymbols("SPY", 
+           from = from, 
+           to = to, 
+           src =  "yahoo", 
+           adjust =  TRUE)
+
+# Use stock() to initialize SPY and set currency to USD
+stock("SPY", currency = "USD")
+
+# Define your trade size and initial equity
+tradesize <- 100000
+initeq <- 100000
+
+# Define the names of your strategy, portfolio and account
+strategy.st <- "firststrat"
+portfolio.st <- "firststrat"
+account.st <- "firststrat"
+
+# Remove the existing strategy if it exists
+rm.strat(strategy.st)
+
+# Initialize the portfolio
+initPortf(portfolio.st, symbols = "SPY", initDate = initdate, currency = "USD")
+
+# Initialize the account
+initAcct(account.st, portfolios = portfolio.st, initDate = initdate, currency = "USD", initEq = initeq)
+
+# Initialize the orders
+initOrders(portfolio.st, initDate = initdate)
+
+# Store the strategy
+strategy(strategy.st, store = TRUE)
